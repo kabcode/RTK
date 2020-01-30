7a4ed73392137d78ad1c44763a0b445c256bb1e5
KabCode <kabcode@gmail.com>
2020-01-30 10:54:01 +0100

    TEST: Add GradientImageFilterTest

--- test/rtkCudaGradientImageFilterTest.cxx
+++ test/rtkCudaGradientImageFilterTest.cxx
@@ -1,19 +1,66 @@
-
+#include "rtkTest.h"
 #include "rtkCudaGradientImageFilter.h"
 
-using Pixeltype = float;
-const unsigned int Dimension = 3;
+#include "itkImageFileReader.h"
+#include "itkConstantBoundaryCondition.h"
 
-using CudaImageType = itk::CudaImage<Pixeltype, Dimension>;
+/**
+ * \file rtkcudagradientimagefiltertest.cxx
+ *
+ * \brief Functional test for gradient computation
+ *
+ *
+ * \author Gordian Kabelitz
+ */
 
-int main(int argc, char* argv[])
+
+int main(int argc, char* argv[] )
 {
-  auto CudaGradientImageFilter = rtk::CudaGradientImageFilter<CudaImageType>::New();
+  constexpr unsigned int Dimension = 3;
+  using PixelType = float;
+  using VectorPixelType = itk::CovariantVector<PixelType, Dimension>;
+  using ImageType = itk::CudaImage<PixelType, Dimension>;
+  using VectorImageType = itk::CudaImage<VectorPixelType, Dimension>;
+  using CudaGradientImageFilterType = rtk::CudaGradientImageFilter<ImageType, float, VectorImageType>;
 
-  auto FixedImage = CudaImageType::New();
-  auto MovingImage = CudaImageType::New();
 
-  std::cout << CudaGradientImageFilter->GetNameOfClass() << std::endl;
+  auto CudaGradientImageFilter = CudaGradientImageFilterType::New();
+
+  auto BoundaryCondition = new itk::ConstantBoundaryCondition<ImageType>();
+  CudaGradientImageFilter->ChangeBoundaryCondition(BoundaryCondition);
+
+  auto FileReader = itk::ImageFileReader<ImageType>::New();
+  FileReader->SetFileName(argv[1]);
+  auto InputVolume = FileReader->GetOutput();
+  InputVolume->Update();
+
+  CudaGradientImageFilter->SetInput(InputVolume);
+  CudaGradientImageFilter->Print(std::cout);
+  try
+  {
+    CudaGradientImageFilter->Update();
+  }
+  catch (itk::ExceptionObject &EO)
+  {
+    EO.Print(std::cout);
+    return EXIT_FAILURE;
+  }
+
+  auto FileWriter = itk::ImageFileWriter<VectorImageType>::New();
+  FileWriter->SetInput(CudaGradientImageFilter->GetOutput());
+  FileWriter->SetFileName("OUTPUT.nrrd");
+
+  try
+  {
+    FileWriter->Update();
+  }
+  catch (itk::ExceptionObject& EO)
+  {
+    EO.Print(std::cout);
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "\n\nTest PASSED! " << std::endl;
 
   return EXIT_SUCCESS;
 }
